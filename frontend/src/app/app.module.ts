b05cd7c3eab18f10bf65d15b78e1c84e2a525bcb
MarvinAsmen <marvin.arduc@gmail.com>
2022-05-08 23:30:37 +0200

    [#59] Implemented basic desktop and mobile menu

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,18 +5,18 @@ import {HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {MessageComponent} from './components/message/message.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {httpInterceptorProviders} from './interceptors';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavigationComponent,
     FooterComponent,
     HomeComponent,
     LoginComponent,
---
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  // toggle extended mobile menu to hide/show
+  showMobileMenu = false;
+
+  // TODO Add router links to different pages
+  routerLinks = {
+    home: "/",
+    logIn: "/login",
+    signUp: "",
+    editProfile: "",
+    search: "",
+    news: "",
+    topTen: "",
+    cart: "",
+    reservations: "",
+    adminTools: ""
+  }
+
+
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {}
+
+  openMobileMenu() {
+    // disables scrollbar when mobile menu is expanded
+    if(this.showMobileMenu){
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+    this.showMobileMenu = !this.showMobileMenu;
+  }
+
+}
