374bda153953e4fd9c48aba286ea4625baf177ba
David Jahn <e12020634@student.tuwien.ac.at>
2022-05-09 16:52:07 +0200

    [52] added 1000 customers and 10 admins as test data

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/UserDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/datagenerator/UserDataGenerator.java
@@ -23,6 +23,22 @@ public class UserDataGenerator {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private static final int NUMBER_OF_USERS_TO_GENERATE = 1000;
+    private static final int NUMBER_OF_ADMINS_TO_GENERATE = 10;
+    private static final String TEST_USER_FIRST_NAME = "Firstname User #";
+    private static final String TEST_USER_LAST_NAME = "Lastname User #";
+    private static final String TEST_USER_EMAIL_PREFIX = "user";
+    private static final String TEST_EMAIL_POSTFIX = "@example.com";
+    private static final String TEST_PASSWORD = "password";
+    private static final String TEST_ADDRESS_STREET = "Getreidemarkt #";
+    private static final String TEST_ADDRESS_ZIP_CODE = "1010";
+    private static final String TEST_ADDRESS_CITY = "Vienna";
+    private static final Country TEST_ADDRESS_COUNTRY = Country.AT;
+
+    private static final String TEST_ADMIN_FIRST_NAME = "Firstname Admin #";
+    private static final String TEST_ADMIN_LAST_NAME = "Lastname Admin #";
+    private static final String TEST_ADMIN_EMAIL_PREFIX = "admin";
+
 
     public UserDataGenerator(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
@@ -30,39 +46,67 @@ public class UserDataGenerator {
     }
 
     @PostConstruct
-    private void generateMessage() {
+    private void generateUserData() {
         if (userRepository.findAll().size() > 0) {
             LOGGER.debug("User already generated");
         } else {
-            LOGGER.debug("Generating 2 User entities");
-            Address address = new Address();
-            address.setStreet("Getreidemarkt 7");
-            address.setZipCode("1010");
-            address.setCity("Vienna");
-            address.setCountry(Country.AT);
+            LOGGER.debug("Generating {} User entities and {} admin entities", NUMBER_OF_USERS_TO_GENERATE, NUMBER_OF_ADMINS_TO_GENERATE);
+            Address address;
+            User user, admin;
+            for (int i = 0; i < NUMBER_OF_USERS_TO_GENERATE; i++) {
+                address = new Address();
+                address.setStreet(TEST_ADDRESS_STREET + i);
+                address.setZipCode(TEST_ADDRESS_ZIP_CODE);
+                address.setCity(TEST_ADDRESS_CITY);
+                address.setCountry(TEST_ADDRESS_COUNTRY);
 
-            User user = new User();
-            user.setFirstName("Hans");
-            user.setLastName("Gittenberger");
-            user.setEmail("user@email.com");
-            user.setPassword(passwordEncoder.encode("password"));
-            user.setAddress(address);
-            user.setRole(UserRole.CUSTOMER);
-            user.setStatus(UserStatus.OK);
-            user.setLastNewsRead(LocalDateTime.now());
-            User admin = new User();
-            admin.setFirstName("Administrator");
-            admin.setLastName("der Terminator");
-            admin.setEmail("admin@email.com");
-            admin.setPassword(passwordEncoder.encode("password"));
-            admin.setAddress(address);
-            admin.setRole(UserRole.ADMIN);
-            admin.setStatus(UserStatus.OK);
-            admin.setLastNewsRead(LocalDateTime.now());
+                user = new User();
+                user.setFirstName(TEST_USER_FIRST_NAME + i);
+                user.setLastName(TEST_USER_LAST_NAME + i);
+                user.setEmail(TEST_USER_EMAIL_PREFIX + i + TEST_EMAIL_POSTFIX);
+                user.setPassword(passwordEncoder.encode(TEST_PASSWORD));
+                user.setAddress(address);
+                user.setRole(UserRole.CUSTOMER);
+                user.setStatus(UserStatus.OK);
+                user.setLastNewsRead(LocalDateTime.now());
+                userRepository.save(user);
+            }
+            for (int i = 0; i < NUMBER_OF_ADMINS_TO_GENERATE; i++) {
+                address = new Address();
+                address.setStreet(TEST_ADDRESS_STREET + (NUMBER_OF_USERS_TO_GENERATE + i));
+                address.setZipCode(TEST_ADDRESS_ZIP_CODE);
+                address.setCity(TEST_ADDRESS_CITY);
+                address.setCountry(TEST_ADDRESS_COUNTRY);
 
-
-            userRepository.save(user);
-            userRepository.save(admin);
+                admin = new User();
+                admin.setFirstName(TEST_ADMIN_FIRST_NAME + i);
+                admin.setLastName(TEST_ADMIN_LAST_NAME + i);
+                admin.setEmail(TEST_ADMIN_EMAIL_PREFIX + i + TEST_EMAIL_POSTFIX);
+                admin.setPassword(passwordEncoder.encode(TEST_PASSWORD));
+                admin.setAddress(address);
+                admin.setRole(UserRole.ADMIN);
+                admin.setStatus(UserStatus.OK);
+                admin.setLastNewsRead(LocalDateTime.now());
+                userRepository.save(admin);
+            }
+//            address = new Address();
+//            address.setStreet("Getreidemarkt 7");
+//            address.setZipCode("1010");
+//            address.setCity("Vienna");
+//            address.setCountry(Country.AT);
+//
+//            User user = new User();
+//            user.setFirstName("Hans");
+//            user.setLastName("Gittenberger");
+//            user.setEmail("user@email.com");
+//            user.setPassword(passwordEncoder.encode("password"));
+//            user.setAddress(address);
+//            user.setRole(UserRole.CUSTOMER);
+//            user.setStatus(UserStatus.OK);
+//            user.setLastNewsRead(LocalDateTime.now());
+//
+//
+//            userRepository.save(admin);
         }
 
     }
