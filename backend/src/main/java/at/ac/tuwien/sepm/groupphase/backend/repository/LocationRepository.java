b93e65cb36869e7734e83d325a854b64bfa883fc
David Jahn <e12020634@student.tuwien.ac.at>
2022-05-13 18:44:05 +0200

    [#20] fixed styling issues; added @Repository annotation; deleted duplicate code

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/LocationRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/LocationRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.stream.Stream;
 import java.util.List;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
     /**
