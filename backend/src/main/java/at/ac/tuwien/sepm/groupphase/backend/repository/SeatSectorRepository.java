769b541a014dfd01c02e46d3ce4aa34cd5c65dc5
MarvinAsmen <marvin.arduc@gmail.com>
2022-05-14 21:05:02 +0200

    [#7] added order data generator

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/SeatSectorRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/SeatSectorRepository.java
@@ -1,9 +1,17 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
+import at.ac.tuwien.sepm.groupphase.backend.entity.Seat;
 import at.ac.tuwien.sepm.groupphase.backend.entity.SeatSector;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SeatSectorRepository extends JpaRepository<SeatSector, Long> {
+
+    @Query("select s from SeatSector s, Performance p where s.location.id = p.location.id and p.id = ?1")
+    List<SeatSector> findByPerformanceId(Long performanceId);
+
 }
