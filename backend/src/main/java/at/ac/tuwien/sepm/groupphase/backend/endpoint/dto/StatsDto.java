ad899a9d909b3da1cb96eac09089a13c45086de3
MarvinAsmen <marvin.arduc@gmail.com>
2022-06-07 09:49:38 +0200

    [#89] BE | Changed StatsDto constructor to @AllArgsConstructor

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/StatsDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/StatsDto.java
@@ -1,17 +1,13 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StatsDto {
     private long tickets;
     private long events;
     private long news;
-
-    public StatsDto(long tickets, long events, long news) {
-        this.tickets = tickets;
-        this.events = events;
-        this.news = news;
-    }
 }
 
