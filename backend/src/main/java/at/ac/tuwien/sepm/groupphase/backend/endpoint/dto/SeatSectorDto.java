5aaef5cf7c83311e55cb53fd46fa2132c4d463c1
Tobias Grantner <tobias.grantner@gmail.com>
2022-06-25 12:47:40 +0200

    [#97] BE | Removed style violation in SeatSectorDto.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/SeatSectorDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/SeatSectorDto.java
@@ -3,7 +3,6 @@ package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.SectorMapper;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Seat;
 import at.ac.tuwien.sepm.groupphase.backend.entity.SeatSector;
-import at.ac.tuwien.sepm.groupphase.backend.entity.Sector;
 import at.ac.tuwien.sepm.groupphase.backend.enums.SeatType;
 import at.ac.tuwien.sepm.groupphase.backend.enums.SectorType;
 import lombok.Data;
@@ -26,7 +25,7 @@ public class SeatSectorDto extends SectorDto {
     @Override
     public SeatSector mapToEntity(SectorMapper mapper) {
         SeatSector sector = mapper.seatSectorDtoToSeatSector(this);
-        for(Seat seat : sector.getSeats()) {
+        for (Seat seat : sector.getSeats()) {
             seat.setSector(sector);
         }
         return sector;
