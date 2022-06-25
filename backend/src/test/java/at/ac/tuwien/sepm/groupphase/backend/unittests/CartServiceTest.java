8b2d740712f1bef43624012fd365c15cd5a2b1ef
MarvinAsmen <marvin.arduc@gmail.com>
2022-06-25 20:40:41 +0200

    [#9] BE | CartServiceTest | Added tests for every public service method of CartService

---
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/unittests/CartServiceTest.java
@@ -0,0 +1,185 @@
+package at.ac.tuwien.sepm.groupphase.backend.unittests;
+
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.CreateTicketDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.PagedTicketsDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.TicketDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.TicketMapper;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Performance;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Seat;
+import at.ac.tuwien.sepm.groupphase.backend.entity.SeatSector;
+import at.ac.tuwien.sepm.groupphase.backend.entity.SeatTicket;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Ticket;
+import at.ac.tuwien.sepm.groupphase.backend.entity.TicketOrder;
+import at.ac.tuwien.sepm.groupphase.backend.entity.User;
+import at.ac.tuwien.sepm.groupphase.backend.enums.OrderType;
+import at.ac.tuwien.sepm.groupphase.backend.enums.SectorType;
+import at.ac.tuwien.sepm.groupphase.backend.repository.OrderRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.PerformanceRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.SeatSectorRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.UserRepository;
+import at.ac.tuwien.sepm.groupphase.backend.service.CartService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class CartServiceTest {
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PerformanceRepository performanceRepository;
+
+    @Autowired
+    private SeatSectorRepository seatSectorRepository;
+
+    @Autowired
+    private TicketMapper ticketMapper;
+
+    @Test
+    @Rollback
+    @Transactional
+    public void givenCart_whenAddTicketsToCart_thenCartIncreased() {
+        // GIVEN
+        User user = userRepository.findUserByEmail("Dora.Dach0@example.com");
+        int before = cartService.getCart(user.getId()).getTickets().size();
+
+        // WHEN
+        List<CreateTicketDto> tickets = new ArrayList<>();
+        CreateTicketDto ticketDto = new CreateTicketDto();
+        ticketDto.setType(SectorType.SEAT);
+        ticketDto.setPerformance(1L);
+        ticketDto.setItem(1L);
+        tickets.add(ticketDto);
+        cartService.addTicketsToCart(user.getId(), tickets);
+
+        // THEN
+        assertEquals(cartService.getCart(user.getId()).getTickets().size(), (before + 1));
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    public void givenNotEmptyCart_whenRemoveTicket_thenCartDecreased() {
+        // GIVEN
+        User user = userRepository.findUserByEmail("Dora.Dach0@example.com");
+        int before = cartService.getCart(user.getId()).getTickets().size();
+        assertTrue(before > 0);
+
+        // WHEN
+        cartService.removeTicket(user.getId(), 1L);
+
+        // THEN
+        assertEquals(cartService.getCart(user.getId()).getTickets().size(), (before - 1));
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    public void givenTicketsInCart_whenGetCart_thenShowTicketsInCart() {
+        // GIVEN
+        User user = userRepository.findUserByEmail("Dora.Dach0@example.com");
+        TicketOrder order = generateOrder(user, OrderType.CART, false, 5);
+
+        // WHEN
+        TicketOrder cart = cartService.getCart(user.getId());
+
+        // THEN
+        assertTrue(cart.getTickets().containsAll(order.getTickets()));
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    public void givenUpcomingPurchasedTickets_whenGetUpcomingPurchasedTickets_thenShowUpcomingPurchasedTickets() {
+        // GIVEN
+        User user = userRepository.findUserByEmail("Dora.Dach0@example.com");
+        TicketOrder order = generateOrder(user, OrderType.PURCHASE, false, 5);
+
+        assertEquals(5, order.getTickets().size());
+
+        // WHEN
+        List<Ticket> upcoming = cartService.getUpcomingPurchasedTickets(user.getId());
+
+        // THEN
+        assertTrue(upcoming.containsAll(order.getTickets()));
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    public void givenPastPurchasedTickets_whenGetPastPurchasedTickets_thenShowPastPurchasedTickets() {
+        // GIVEN
+        User user = userRepository.findUserByEmail("Dora.Dach0@example.com");
+        TicketOrder order = generateOrder(user, OrderType.PURCHASE, true, 6);
+
+        List<TicketDto> tickets = order.getTickets().stream().map(ticketMapper::ticketToTicketDto).toList();
+
+        // WHEN
+        PagedTicketsDto past = cartService.getPastPurchasedTickets(user.getId(), 0, 6);
+
+        // THEN
+        assertTrue(past.getTickets().containsAll(tickets));
+    }
+
+    private TicketOrder generateOrder(User user, OrderType type, boolean past, int numberOfTickets) {
+        LocalDateTime now = LocalDateTime.now();
+
+        Performance performance = performanceRepository.findById(1L).get();
+        performance.setDateTime((past) ? now.minusYears(1) : now.plusYears(1));
+        performanceRepository.save(performance);
+
+        TicketOrder order = new TicketOrder();
+        order.setType(type);
+        order.setDateTime(now);
+        order.setValidUntil(now.plusHours(1));
+        order.setUser(user);
+        order.setUserId(user.getId());
+        orderRepository.save(order);
+
+        List<Ticket> tickets = new ArrayList<>();
+        List<SeatSector> seatSectors = seatSectorRepository.findByPerformanceId(performance.getId());
+        int counter = 0;
+        for (SeatSector sector : seatSectors) {
+            for (Seat seat : sector.getSeats()) {
+                SeatTicket ticket = new SeatTicket();
+                ticket.setPerformanceId(performance.getId());
+                ticket.setPerformance(performance);
+                ticket.setSectorId(sector.getId());
+                ticket.setSeatId(seat.getId());
+                ticket.setOrderId(order.getId());
+                tickets.add(ticket);
+                if (counter == numberOfTickets) {
+                    break;
+                } else counter++;
+            }
+            if (counter == numberOfTickets) {
+                break;
+            }
+        }
+
+        order.setTickets(tickets.subList(0, numberOfTickets));
+        orderRepository.save(order);
+
+        return order;
+    }
+
+}
